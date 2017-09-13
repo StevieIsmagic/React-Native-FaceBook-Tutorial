746042010e22bc3ccb7d61f0ec30dec13d338212
Colombianito66 <colombianito66@gmail.com>
2017-09-13 02:54:52 -0400

    Align Items Basics Practice

--- FB-Guide/App.js
+++ FB-Guide/App.js
@@ -6,12 +6,13 @@ import { View } from 'react-native';
       return (
         <View style={{
           flex: 1, 
-          flexDirection: 'row',
-          justifyContent: 'space-around',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'stretch',
           }}>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{height: 50, backgroundColor: 'powderblue'}} />
           <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+          <View style={{height: 50, backgroundColor: 'steelblue'}} />
         </View>
       );
     }
