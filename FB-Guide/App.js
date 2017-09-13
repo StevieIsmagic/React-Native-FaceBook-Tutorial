a580cba58bc6dec9e3be35262f8630f97bcfd4cd
Colombianito66 <colombianito66@gmail.com>
2017-09-13 00:18:28 -0400

    Setting State Practice

--- FB-Guide/App.js
+++ FB-Guide/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-class Greeting extends Component {
-  render() {   
-    return (
-      <Text> Hello {this.props.name} !? </Text>
-    );
-  }
-}
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
 
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center'}}> 
-        <Greeting name={'Stevie'} />
-        <Greeting name={'Vanessa'} />
-      </View>
-    );
+    //Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
   }
-}
+  
+    render() {
+      let display = this.state.showText ? this.props.text : ' ';
+      return (
+        <Text> {display} </Text>
+      );
+    }
+  }
+
+  export default class BlinkApp extends Component {
+    render() {
+      return (
+        <View>
+          <Blink text={'I Love You'} />
+          <Blink text={'I Love You'} />
+          <Blink text={'I Love You'} />
+          <Blink text={'I Love You'} />
+          <Blink text={'I Love You'} />
+          <Blink text={'I Love You'} />
+        </View>
+      )
+    }
+  }
