bdd275230f37cc41d84529e1d81bbc81bb0d9651
Colombianito66 <colombianito66@gmail.com>
2017-09-13 02:23:38 -0400

    Flexbox Practice

--- FB-Guide/App.js
+++ FB-Guide/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-  export default class FixedDimensionBasics extends Component {
+  export default class FlexDirectionBasics extends Component {
     render() {
       return (
-        <View style={{flex: 1}}>
-          <View style={{flex:2, backgroundColor: 'powderblue'}} />
-          <View style={{flex:2, backgroundColor: 'skyblue'}} />
-          <View style={{flex:2, backgroundColor: 'steelblue'}} />
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
         </View>
       );
     }
