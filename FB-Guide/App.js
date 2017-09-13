6e005a5b694d410d571ebcb801ad3c4c7e0c0f75
Colombianito66 <colombianito66@gmail.com>
2017-09-12 23:01:54 -0400

    expo running on ipad client

---
+++ FB-Guide/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>     
+        <Text style={styles.child}>1</Text>  
+        <Text style={styles.child}> HIIIII</Text>
+
+        <Text style={styles.child}>"2" </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  child: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'white',
+  }
+});
