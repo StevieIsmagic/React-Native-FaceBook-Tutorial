54e6d366df99d39ab267531143356685fa72a0fc
Colombianito66 <colombianito66@gmail.com>
2017-09-12 23:21:59 -0400

    Text compnent practice

--- FB-Guide/App.js
+++ FB-Guide/App.js
@@ -1,28 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text } from 'react-native';
 
-export default class App extends React.Component {
+export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={styles.container}>     
-        <Text style={styles.child}>1</Text>  
-        <Text style={styles.child}> HIIIII</Text>
-        <Text style={styles.child}>"2" </Text>
-      </View>
+      <Text> Hello WoRld! </Text>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  child: {
-    height: 100,
-    width: 100,
-    backgroundColor: 'white',
-  }
-});
