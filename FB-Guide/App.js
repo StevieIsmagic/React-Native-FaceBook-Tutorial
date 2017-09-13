5ece173fa3eb47d0f64c670c14cf5035bd8adf19
Colombianito66 <colombianito66@gmail.com>
2017-09-13 02:36:13 -0400

    Flexbox Justify Content Practice

--- FB-Guide/App.js
+++ FB-Guide/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-  export default class FlexDirectionBasics extends Component {
+  export default class JustifyContentBasics extends Component {
     render() {
       return (
-        <View style={{flex: 1, flexDirection: 'column'}}>
+        <View style={{
+          flex: 1, 
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          }}>
           <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
           <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
           <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
